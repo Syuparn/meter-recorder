b3f9d84842f8150e78bb96020c234290e166f69f
syuparn <s.hello.spagetti@gmail.com>
2022-07-15 22:37:29 +0900

    fix config

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,7 +5,7 @@ export class Config {
     public readonly switchbot_auth_token: string,
   ) {
     this.switchbot_endpoint =
-      switchbot_endpoint || 'https://api.switch-bot.com/v1.0/';
+      switchbot_endpoint || 'https://api.switch-bot.com/v1.0';
     this.meter_device_id = meter_device_id;
     this.switchbot_auth_token = switchbot_auth_token;
   }
--- src/config/gasConfig.test.ts
+++ src/config/gasConfig.test.ts
@@ -26,7 +26,7 @@ test.each([
       meter_device_id: null,
       switchbot_auth_token: null,
     },
-    expected: new Config('https://api.switch-bot.com/v1.0/', '', ''),
+    expected: new Config('https://api.switch-bot.com/v1.0', '', ''),
   },
   {
     name: 'empty string is treated like null',
@@ -35,7 +35,7 @@ test.each([
       meter_device_id: '',
       switchbot_auth_token: '',
     },
-    expected: new Config('https://api.switch-bot.com/v1.0/', '', ''),
+    expected: new Config('https://api.switch-bot.com/v1.0', '', ''),
   },
 ])('$name', ({ properties, expected }) => {
   const configFactory = new GASConfigFactory(new DummyProperties(properties));
